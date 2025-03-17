7f29669fde12152e9d8aeabfd40a6aafdba5fcca
TinnTinnn <144338656+TinnTinnn@users.noreply.github.com>
2025-03-17 17:08:13 +0700

    Fix missing Log facade import

--- la-react/app/Http/Controllers/ProfilePictureController.php
+++ la-react/app/Http/Controllers/ProfilePictureController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Member;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class ProfilePictureController extends Controller

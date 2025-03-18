e876c59d13f4cb98377de664dbcf9837c235973e
TinnTinnn <144338656+TinnTinnn@users.noreply.github.com>
2025-03-18 16:58:52 +0700

    Againnn!!

--- react-la/vite.config.js
+++ react-la/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // output เป็น dist
+    outDir: 'dist',
   },
   publicDir: 'public',
   server: {

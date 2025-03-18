a636ad5ca5c08401e2a1372247ed05b8592d7b64
TinnTinnn <144338656+TinnTinnn@users.noreply.github.com>
2025-03-18 15:23:14 +0700

    Update vite.config.js

--- react-la/vite.config.js
+++ react-la/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // output เป็น dist
+  },
+  publicDir: 'public',
   server: {
     proxy: {
       '/api': {

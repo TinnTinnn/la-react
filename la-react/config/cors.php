3aaa4d43e5f8b482629508329c5c364b35b0c9e1
TinnTinnn <144338656+TinnTinnn@users.noreply.github.com>
2025-03-26 16:54:02 +0700

    Edit .cors for local dev can work properly

--- la-react/config/cors.php
+++ la-react/config/cors.php
@@ -1,13 +1,16 @@
 <?php
 
 return [
-    'paths'                => ['*'],
-    'allowed_methods'      => ['*'],
-    'allowed_origins'      => [
+    'paths'                    => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods'          => ['*'],
+    'allowed_origins'          => [
         'https://react-la.onrender.com', // URL ของ frontend
+        'http://localhost:5173',         // เพิ่มบรรทัดนี้
+        'http://127.0.0.1:5173',         // เพิ่มบรรทัดนี้
     ],
-    'allowed_headers'      => ['*'],
-    'exposed_headers'      => [],
-    'max_age'              => 0,
-    'supports_credentials' => true,
+    'allowed_origins_patterns' => [],
+    'allowed_headers'          => ['*'],
+    'exposed_headers'          => [],
+    'max_age'                  => 0,
+    'supports_credentials'     => true,
 ];
